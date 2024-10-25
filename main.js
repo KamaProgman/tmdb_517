2ab34431a41eeef3f3aaf392916dc7a20c41f46d
KamaProgman <latipovkamran1@gmail.com>
2024-10-25 18:31:53 +0500

    layout

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 4,
-    spaceBetween: 30,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    breakpoints: {
-        640: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-        },
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        1024: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-        },
-        1280: {
-            slidesPerView: 4,
-            spaceBetween: 30,
-        },
-    },
+var swiper = new Swiper(".swiper-container", {
+	slidesPerView: 4,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	freeMode: true,
+	breakpoints: {
+		640: {
+			slidesPerView: 1,
+			spaceBetween: 20,
+		},
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+		},
+		1024: {
+			slidesPerView: 3,
+			spaceBetween: 30,
+		},
+		1280: {
+			slidesPerView: 4,
+			spaceBetween: 30,
+		},
+	},
 });
