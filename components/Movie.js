5917b314712a841b5af63e638343dd7a53a22bba
KamaProgman <latipovkamran1@gmail.com>
2024-10-28 18:29:29 +0500

    image fix

---
+++ components/Movie.js
@@ -0,0 +1,36 @@
+import { imageBaseUrl } from "../libs/utils";
+
+export function Movie(item) {
+	console.log(item);
+
+	const itemDiv = document.createElement("div");
+	itemDiv.className = "item";
+
+	const imgBoxDiv = document.createElement("div");
+	imgBoxDiv.className = "img_box";
+
+	const imgElement = document.createElement("img");
+	imgElement.src = imageBaseUrl + item.poster_path;
+	imgElement.alt = "movie";
+
+	const ratingSpan = document.createElement("span");
+	ratingSpan.className = "reyting";
+	ratingSpan.textContent = item.vote_average;
+
+	imgBoxDiv.appendChild(imgElement);
+	imgBoxDiv.appendChild(ratingSpan);
+
+	const nameParagraph = document.createElement("p");
+	nameParagraph.className = "cinema_name";
+	nameParagraph.textContent = item.title;
+
+	const genreParagraph = document.createElement("p");
+	genreParagraph.className = "cinema_genre";
+	genreParagraph.textContent = item.original_title;
+
+	itemDiv.appendChild(imgBoxDiv);
+	itemDiv.appendChild(nameParagraph);
+	itemDiv.appendChild(genreParagraph);
+
+	return itemDiv;
+}
--- main.js
+++ main.js
@@ -1,43 +1,54 @@
-// import Swiper from "swiper";
-// import "swiper/css";
+import Swiper from "swiper";
+import "swiper/css";
 // import { popularApi } from "./api/movie";
 
 import { nowPlayingApi, popularApi, upcomingApi } from "./api/movie";
-import { Nowplaying } from "./components/Nowplaing";
-import { UpcomingMovie } from "./components/UpcomingMovie";
 import { reload } from "./libs/utils";
-
-// const swiper = new Swiper(".swiper", {
-// 	slidesPerView: 4,
-// 	pagination: {
-// 		el: ".swiper-pagination",
-// 		clickable: true,
-// 	},
-// 	freeMode: true,
-// 	breakpoints: {
-// 		640: {
-// 			slidesPerView: 1,
-// 			spaceBetween: 20,
-// 		},
-// 		768: {
-// 			slidesPerView: 2,
-// 			spaceBetween: 20,
-// 		},
-// 		1024: {
-// 			slidesPerView: 3,
-// 			spaceBetween: 30,
-// 		},
-// 		1280: {
-// 			slidesPerView: 4,
-// 			spaceBetween: 30,
-// 		},
-// 	},
-// });
+import { Movie } from "./components/Movie";
 
 nowPlayingApi()
-	.then((res) => reload(res.data.results.slice(0, 8), document.querySelector('.mn_center_item'), Nowplaying))
+	.then((res) =>
+		reload(
+			res.data.results.slice(0, 8),
+			document.querySelector(".mn_center_item"),
+			Movie
+		)
+	)
 	.catch((error) => console.error(error));
 
 upcomingApi()
-	.then((res) => reload(res.data.results.slice(0, 4), document.querySelector('.pictures'), UpcomingMovie))
+	.then((res) =>
+		reload(
+			res.data.results.slice(0, 4),
+			document.querySelector(".pictures"),
+			Movie
+		)
+	)
 	.catch((error) => console.error(error));
+
+const swiper = new Swiper(".swiper", {
+	slidesPerView: 4,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	freeMode: true,
+	breakpoints: {
+		640: {
+			slidesPerView: 1,
+			spaceBetween: 20,
+		},
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+		},
+		1024: {
+			slidesPerView: 3,
+			spaceBetween: 30,
+		},
+		1280: {
+			slidesPerView: 4,
+			spaceBetween: 30,
+		},
+	},
+});
