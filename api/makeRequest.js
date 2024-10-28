321ec28c316fac6b3652d87cb865240e69ee3dca
Daler-433 <dalerjon433ishpulatov@gmail.com>
2024-10-28 07:58:49 -0400

    fhfh

--- api/makeRequest.js
+++ api/makeRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let accessToken =
-	"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MDExNmY3NmEwZWJjNTQ3NzE5ZTg1NmQ4NmQ5MmY5ZSIsIm5iZiI6MTcyOTM0ODU1MS41MjkyMzgsInN1YiI6IjY2MzYyMjNjNDcwZWFkMDEyMjEyM2UxZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VSDYPQCXFklnmVuE5Imj5GFKbr_AAMgrXOZtewdKjgo";
+	"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZjY2MGZiMDc0YzQ3M2UzOTU4N2E4NWI1YzliMmFjOSIsIm5iZiI6MTczMDExNjEyMS41ODcyNDksInN1YiI6IjY3MTkwOTg0YjNkNWNiYjg0MmYzYmZhMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wlrC83qTD2VPeBKChkDmj6pmwd__emo6p6CFsC0VOQE";
 
 export async function makeRequest(url, params, method = "GET", body = {}) {
 	try {
